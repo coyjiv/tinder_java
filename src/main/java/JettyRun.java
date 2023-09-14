2e524f65f0b327e23191257a24c0f39dd1cf4d8d
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2023-09-14 21:59:29 +0200

    added bootstrap

--- src/main/java/JettyRun.java
+++ src/main/java/JettyRun.java
@@ -1,5 +1,4 @@
-import controller.IndexServlet;
-import controller.TemplateEngine;
+import controller.*;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -20,16 +19,17 @@ public class JettyRun
         ServletContextHandler handler = new ServletContextHandler();
 
         IndexServlet indexServlet = new IndexServlet(templateEngine);
+        UsersServlet usersServlet = new UsersServlet(templateEngine);
 
 
         handler.addServlet(new ServletHolder(indexServlet), "/");
 //        handler.addServlet(new ServletHolder(helloServlet), "/hello");
-//        handler.addServlet(new ServletHolder(usersServlet), "/users");
+        handler.addServlet(new ServletHolder(usersServlet), "/users");
+        handler.addServlet(CSSBootstrapServlet.class, "/css/bootstrap.min.css");
 //        handler.addServlet(new ServletHolder(countriesServlet), "/countries");
 //        handler.addServlet(new ServletHolder(logoutServlet), "/logout");
 //        handler.addFilter(new FilterHolder(new LoginFilter(templateEngine, userService)), "/*", EnumSet.of(DispatcherType.REQUEST));
-//        handler.addServlet(CSSBootstrapServlet.class, "/css/bootstrap.min.css");
-//        handler.addServlet(JsBootstrapServlet.class, "/js/bootstrap.min.css");
+        handler.addServlet(JsBootstrapServlet.class, "/js/bootstrap.min.js");
         server.setHandler(handler);
 
         server.start();
---
+++ src/main/java/controller/UsersServlet.java
@@ -0,0 +1,44 @@
+package controller;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+public class UsersServlet extends HttpServlet {
+    private final TemplateEngine templateEngine;
+//    private final UserService userService;
+
+    public UsersServlet(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+//        this.userService = userService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+//        Dotenv dotenv = Dotenv.load();
+//        String dbLogin = dotenv.get("DB_LOGIN");
+//
+//        String dbConnectionString = dotenv.get("DB_CONNECTION_URL");
+//        String dbPassword = dotenv.get("DB_PASSWORD");
+//        try (Connection connection = DriverManager.getConnection(dbConnectionString, dbLogin, dbPassword)) {
+//            Statement statement = connection.createStatement();
+//
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM \"test-table\"");
+//
+//            while (resultSet.next()) {
+//
+//                String name = resultSet.getString("name");
+//                System.out.println("test name output : " + name);
+//            }
+//
+//        } catch (SQLException e){
+//            System.out.println("Something went wrong, with db interaction returning countries");
+//            e.printStackTrace();
+//        }
+        templateEngine.render("like-page.ftl", response);
+    }
+
+}
