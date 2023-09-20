00f2f7d07196c5b5b691fae27fe2494a015e2d00
Vladimirov2077 <blekvajv@gmail.com>
2023-09-20 20:54:45 +0200

    /liked + user , people-list.ftl

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,5 +1,27 @@
 package domain;
 
 public class User {
+String avatarUrl;
+String username;
 
+public User(String avatarUrl, String username){
+    this.avatarUrl = avatarUrl;
+    this.username = username;
+}
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
