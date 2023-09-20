5cedd79cd1152985a2624194a7efe25545180626
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2023-09-21 00:26:56 +0300

    login & logout

--- src/main/java/controller/IndexServlet.java
+++ src/main/java/controller/IndexServlet.java
@@ -2,13 +2,16 @@ package controller;
 
 
 import io.github.cdimascio.dotenv.Dotenv;
+import utils.CookieUtil;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class IndexServlet extends HttpServlet {
     private final TemplateEngine templateEngine;
@@ -31,22 +34,27 @@ public class IndexServlet extends HttpServlet {
 
         String dbConnectionString = dotenv.get("DB_CONNECTION_URL");
         String dbPassword = dotenv.get("DB_PASSWORD");
+
+        long sessionUserId = -1;
+        if (CookieUtil.findCookieByName(request, LoginFilter.SESSION_USER_ID).isPresent()){
+            sessionUserId = Long.parseLong(CookieUtil.findCookieByName(request, LoginFilter.SESSION_USER_ID).get().getValue());
+        }
         try (Connection connection = DriverManager.getConnection(dbConnectionString, dbLogin, dbPassword)) {
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM \"test-table\"");
-
-            while (resultSet.next()) {
-
-                String name = resultSet.getString("name");
-                System.out.println("test name output : " + name);
+            PreparedStatement statement = connection.prepareStatement("SELECT username FROM users WHERE user_id = ?");
+            statement.setLong(1,sessionUserId);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()) {
+                String name = resultSet.getString("username");
+                Map<String, Object> params = Map.of(
+                        "userName", name
+                );
+                templateEngine.render("dashboard.ftl", params,response);
             }
 
         } catch (SQLException e){
             System.out.println("Something went wrong, with db interaction returning countries");
             e.printStackTrace();
         }
-        templateEngine.render("dashboard.ftl", response);
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
